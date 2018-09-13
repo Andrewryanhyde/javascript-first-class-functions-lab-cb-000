42d38387543ea68fd3974d5faa66f572338e9d59
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-09-13 10:34:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,7 @@ function multiplier(a,b) {
   return a * b;
 }
 
-var doubler = createMultiplier(2)
-var tripler = createMultiplier(3)
+var doubler = createMultiplier(2);
+var tripler = createMultiplier(3);
+var doublerWithBind = multiplier.bind(null, 2);
+var triplerWithBind = multiplier.bind(null, 3);
